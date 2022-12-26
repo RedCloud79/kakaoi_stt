fd8aa80d143849c3fe472d5601e666439342a84a
RedCloud79 <120488365+RedCloud79@users.noreply.github.com>
2022-12-26 16:53:39 +0900

    Initial commit

---
+++ kakaoi_autosound.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+# 음성 데이터는 Mono Channel 16000 Hz samplerate 16bit depth의 rawpcm포맷만 지원.
+
+#sample rate : 현실 세계의 아날로그 소리를 잘게 쪼갠 비율(속도). 잘게 쪼개진 하나를 샘플이라 부른다.
+#  == 1초당 추출되는 샘플 개수 ex:) 44100 Hz - 1초당 44100개의 샘플
+#- channel : 스피커 수와 연관 있다.
+#- bits per sample : 하나의 샘플을 표현하기 위해 사용되는 bit 수.
+#- bit rate : 1초당 비트 전송 수. (sample rate * channel * bits per sample)
+#- RAW PCM 포맷 :  PCM(Pulse code modulation)로 표현한 오디오 데이터를 압축하지 않은 RAW 형태(wav 등)
+
+#stt파트
+kakao_speech_url = "https://kakaoi-newtone-openapi.kakao.com/v1/recognize" #카카오 개발자서버연결
+
+rest_api_key = '1b547c7fe1240a9e7848988391faa360' # 카카오 개발자서버 연결 코드
+
+headers = {
+    "Content-Type": "application/octet-stream",
+    "X-DSS-Service": "DICTATION",
+    "Authorization": "KakaoAK " + rest_api_key,
+}
+
+def dictation(audio):
+    res = requests.post(kakao_speech_url, headers=headers, data=audio)
+    
+    result_json_string = res.text[
+                         res.text.index('{"type":"finalResult"'):res.text.rindex('}') + 1
+                         ]
+    
+    result = json.loads(result_json_string)
+    return result['value']
+
+if __name__ == "__main__":
+    with open('rec.wav', 'rb') as fp: #파일명을 설정하여 mp3파일명을 가져옴
+        audio = fp.read()
+        result = dictation(audio)
+        print(result) #text 출력
---
+++ kakaoi_content.py
@@ -0,0 +1,87 @@
+from __future__ import print_function
+
+import pyaudio
+import wave
+import io
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode (GPIO.BCM)
+GPIO.setup (17, GPIO.OUT)
+
+from pydub import AudioSegment
+from kakaoi_autosound import *
+from kakaoi_record import *
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+SAMPLE_WIDTH = 2
+CHANNELS = 1
+RATE = 16000
+
+
+def record(record_seconds=3): # 5초간 녹음기 활성화
+    p = pyaudio.PyAudio()
+    stream = p.open(input_device_index=11,
+                    format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    print("Start to record the audio.") #녹음기 시작 출력 안내
+
+    frames = []
+    for i in range(0, int(RATE / CHUNK * record_seconds)):
+        data = stream.read(CHUNK, exception_on_overflow=False)
+        frames.append(data)
+
+    print("Recording is finished.") #녹음이 끝났다는 출력 안내
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    return frames
+
+
+def convertTo16K(file):
+    # 16000 으로 변환
+    sound = AudioSegment.from_file(file)
+    sound = sound.set_frame_rate(16000)
+    rec_data = io.BytesIO()
+    sound.export(rec_data, format="wav")
+    sound.export("rec.wav", format="wav")
+    return rec_data
+
+
+# wav 파일 저장
+def getWav(frames):
+    wavfile = io.BytesIO()
+    wf = wave.open(wavfile, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(SAMPLE_WIDTH)
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wavfile.seek(0)
+    return wavfile
+
+
+if __name__ == "__main__":
+    audio_data = record()
+    wav_data = getWav(audio_data)
+    wav_data = convertTo16K(wav_data)
+    # wav_data 처리
+    result = dictation (wav_data.getvalue())  # wav 파일 내용 전달
+    print('인식결과', result)
+    #LED 제어
+    if result.find('가디건') !=-1: #'가디건'이 들어왔을 때
+        print('led on')
+        GPIO.output(17, True) #LED를 켭니다.
+        time.sleep(1)
+    if result.find('불 꺼') !=-1: #'불꺼'가 들어왔을 때
+        print('led off')
+        GPIO.output(17, False) #LED를 끕니다.
+        time.sleep(1)
+        GPIO.cleanup()
+
+
